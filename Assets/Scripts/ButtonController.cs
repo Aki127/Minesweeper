eb033eb0d24a586a55b76a078ae2e174760dae27
Akifumi Tsutsui <tti.aki127@gmail.com>
2017-11-01 23:46:26 +0900

    不要ファイルの除外

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour {
+
+		public void GameStart(){
+		SceneManager.LoadScene ("Game");
+	}
+		public void Result(){
+		SceneManager.LoadScene ("Result");
+	}
+		public void Home(){
+		SceneManager.LoadScene ("Start");
+	
+
+
+	
+		
+	}
+}
